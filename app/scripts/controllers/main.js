2b6878e66d32cfa51702c3b7a80cc83b41387e81
Raúl Jiménez <jimenezjrs@gmail.com>
2014-11-26 18:35:43 -0600

    dataset can now be filtered correctly

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,37 @@
  * Controller of the financiamientoClimaticoApp
  */
 angular.module('financiamientoClimaticoApp')
-  .controller('MainCtrl', ['SettingsFactory', function (SettingsFactory) {
+  .controller('MainCtrl', ['$scope', 'Api', function ($scope, Api) {
+    var self = this;
 
-    console.log(SettingsFactory);
-
-    this.years = SettingsFactory.map.yearsOptions;
-    this.financing = SettingsFactory.map.financingOptions;
-    this.focus = SettingsFactory.map.focusOptions;
+    // Initialize all variables
+    this.options = Api.data.options;
+    this.data = Api.data;
 
     this.filters = {
       year: null,
       financing: null,
       focus: null
+    };
+
+    var applyFilterToRecord = function(filterValue, recordPropertyValue) {
+      return  filterValue === null ||
+              angular.equals(recordPropertyValue, filterValue);
     }
+
+    self.filterRecords = function(record) {
+      // console.log(
+      //   applyFilterToRecord(self.filters.year, record.ano_aprobacion) &&
+      //   applyFilterToRecord(self.filters.financing, record.financiamiento) &&
+      //   applyFilterToRecord(self.filters.focus, record.area_proyecto)
+      // );
+      return applyFilterToRecord(self.filters.year, record.ano_aprobacion) &&
+             applyFilterToRecord(self.filters.financing, record.financiamiento) &&
+             applyFilterToRecord(self.filters.focus, record.area_proyecto);
+    };
+
+    // Fetch the data from the api
+    Api.fetchDataset();
+
+
   }]);
--- app/scripts/services/settings.js
+++ app/scripts/services/settings.js
@@ -8,12 +8,14 @@
  * Factory in the financiamientoClimaticoApp.
  */
 angular.module('financiamientoClimaticoApp')
-  .factory('SettingsFactory', [function () {
+  .factory('SettingsFactory', ['Api', function (Api) {
     var map = {
       yearsOptions: [2012, 2013, 2014],
       financingOptions: ['financing_option 1', 'financing_option 2', 'financing_option 3'],
       focusOptions: ['scope_option 1', 'scope_option 2', 'scope_option 3'],
     };
+
+    Api.fetchDataset();
 
     return {
       map: map
